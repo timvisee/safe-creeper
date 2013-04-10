77b47172caf175e64fd5bf8ec304e77c583922f1
Tim Visee <timvisee@gmail.com>
2013-04-10 18:28:16 +0200

    Made SCLogger extends the Logger class
    Made the SCLogger class extends the Logger class.
    Added java docs description to SCLogger methods.

--- src/com/timvisee/safecreeper/SCLogger.java
+++ src/com/timvisee/safecreeper/SCLogger.java
@@ -2,9 +2,7 @@ package com.timvisee.safecreeper;
 
 import java.util.logging.Logger;
 
-public class SCLogger {
-	
-	private Logger log;
+public class SCLogger extends Logger {
 	
 	private final String prefix = "[SafeCreeper] ";
 	private final String errorPrefix = "[SafeCreeper] [ERROR] ";
@@ -13,19 +11,37 @@ public class SCLogger {
 	private boolean defShowErrorPrefix = true;
 	private boolean defShowDebugPrefix = true;
 	
+	/**
+	 * Constructor
+	 * @param name Logger name
+	 * @param resourceBundleName Logger recourse bundle name
+	 */
+	public SCLogger(String name, String resourceBundleName) {
+		super(name, resourceBundleName);
+	}
+	
+	/**
+	 * Constructor
+	 * @param log Logger
+	 */
 	public SCLogger(Logger log) {
-		if(log != null)
-			this.log = log;
-		
-		setup();
+		super(log.getName(), log.getResourceBundleName());
+	}
+
+	/**
+	 * Info
+	 * @param text Text
+	 */
+	public void info(String text) {
+		info(text, this.defShowPrefix);
 	}
 	
-	public void setup() { }
-	
-	public String info(String text) {
-		return info(text, this.defShowPrefix);
-	}
-	
+	/**
+	 * Info
+	 * @param text Text
+	 * @param prefix Prefix
+	 * @return Output
+	 */
 	public String info(String text, boolean prefix) {
 		String msg;
 		
@@ -34,14 +50,24 @@ public class SCLogger {
 		else
 			msg = text;
 		
-		this.log.info(msg);
+		super.info(msg);
 		return msg;
 	}
 	
-	public String error(String text) {
-		return error(text, this.defShowErrorPrefix);
+	/**
+	 * Error
+	 * @param text Text
+	 */
+	public void error(String text) {
+		error(text, this.defShowErrorPrefix);
 	}
 	
+	/**
+	 * Error
+	 * @param text Text
+	 * @param prefix Prefix
+	 * @return Output
+	 */
 	public String error(String text, boolean prefix) {
 		String msg;
 		
@@ -50,14 +76,24 @@ public class SCLogger {
 		else
 			msg = text;
 		
-		this.log.info(msg);
+		super.info(msg);
 		return msg;
 	}
 	
-	public String debug(String text) {
-		return debug(text, this.defShowDebugPrefix);
+	/**
+	 * Debug
+	 * @param text Text
+	 */
+	public void debug(String text) {
+		debug(text, this.defShowDebugPrefix);
 	}
 	
+	/**
+	 * Debug
+	 * @param text Text
+	 * @param prefix Prefix
+	 * @return Output
+	 */
 	public String debug(String text, boolean prefix) {
 		String msg;
 		
@@ -66,52 +102,79 @@ public class SCLogger {
 		else
 			msg = text;
 		
-		this.log.info(msg);
+		super.info(msg);
 		return msg;
 	}
 	
+	/**
+	 * Get the prefix
+	 * @return Prefix
+	 */
 	public String getPrefix() {
 		return this.prefix;
 	}
 	
+	/**
+	 * Get the error prefix
+	 * @return Error prefix
+	 */
 	public String getErrorPrefix() {
 		return this.errorPrefix;
 	}
 	
+	/**
+	 * Get the debug prefix
+	 * @return Debug prefix
+	 */
 	public String getDebugPrefix() {
 		return this.debugPrefix;
 	}
 	
+	/**
+	 * Check if the prefix is shown by default
+	 * @return True if shown by default
+	 */
 	public boolean getShowPrefixByDefault() {
 		return this.defShowPrefix;
 	}
 	
+	/**
+	 * Set if the prefix is shown by default
+	 * @param showPrefix Prefix shown by default
+	 */
 	public void setShowPrefixByDefault(boolean showPrefix) {
 		this.defShowPrefix = showPrefix;
 	}
 	
+	/**
+	 * Get if the error prefix is shown by default
+	 * @return Error prefix shown by default
+	 */
 	public boolean getShowErrorPrefixByDefault() {
 		return this.defShowErrorPrefix;
 	}
 	
+	/**
+	 * Set if the error prefix is shown by default
+	 * @param showErrorPrefix Error prefix shown by default
+	 */
 	public void setShowErrorPrefixByDefault(boolean showErrorPrefix) {
 		this.defShowErrorPrefix = showErrorPrefix;
 	}
 	
+	/**
+	 * Get if the debug prefix is shown by default
+	 * @return True if the debug prefix shown by default
+	 */
 	public boolean getShowDebugPrefixByDefault() {
 		return this.defShowDebugPrefix;
 	}
 	
+	/**
+	 * Set if the debug prefix is shown by default
+	 * @param showDebugPrefix Debug prefix shown by default
+	 */
 	public void setShowDebugPrefixByDefault(boolean showDebugPrefix) {
 		this.defShowErrorPrefix = showDebugPrefix;
-	}
-	
-	public Logger getLogger() {
-		return this.log;
-	}
-	
-	public void setLogger(Logger log) {
-		if(log != null)
-			this.log = log;
 	}
 } 
