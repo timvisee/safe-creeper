806b25b6fe344966c0076623b6bfccdb8b8a5280
Tim Visee <timvisee@gmail.com>
2013-06-29 13:51:16 +0200

    Added SpreadUpwards to the liquid controls
    Added SpreadUpwards to the liquid controls.

--- src/com/timvisee/safecreeper/listener/SCBlockListener.java
+++ src/com/timvisee/safecreeper/listener/SCBlockListener.java
@@ -6,6 +6,7 @@ import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.block.BlockFace;
 import org.bukkit.block.BlockState;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -192,8 +193,26 @@ public class SCBlockListener implements Listener {
 			// This is water, check if water can spread
 			if(!SafeCreeper.instance.getConfigManager().getOptionBoolean(w, "WaterControl", "WaterSpread", true, true, l))
 				event.setCancelled(true);
-			else if(SafeCreeper.instance.getConfigManager().getOptionBoolean(w, "WaterControl", "InfiniteWater", false, true, l))
-				toBlock.setData((byte) 0);
+			else {
+				if(SafeCreeper.instance.getConfigManager().getOptionBoolean(w, "WaterControl", "InfiniteWater", false, true, l))
+					toBlock.setData((byte) 0);
+				
+				if(SafeCreeper.instance.getConfigManager().getOptionBoolean(w, "WaterControl", "SpreadUpwards", false, true, l)) {
+					BlockFace[] faces = new BlockFace[]{
+							BlockFace.NORTH, BlockFace.EAST,
+							BlockFace.SOUTH, BlockFace.WEST};
+					
+					for(BlockFace f : faces) {
+						Block relBlock = toBlock.getRelative(f);
+						Block aboveBlock = relBlock.getRelative(BlockFace.UP);
+						
+						if(!relBlock.isLiquid() && !relBlock.isEmpty()) {
+							if(aboveBlock.isEmpty())
+								aboveBlock.setType(b.getType());
+						}
+					}
+				}
+			}
 			break;
 			
 		case LAVA:
@@ -201,8 +220,26 @@ public class SCBlockListener implements Listener {
 			// This is lava, check if lava can spread
 			if(!SafeCreeper.instance.getConfigManager().getOptionBoolean(w, "LavaControl", "LavaSpread", true, true, l))
 				event.setCancelled(true);
-			else if(SafeCreeper.instance.getConfigManager().getOptionBoolean(w, "LavaControl", "InfiniteLava", false, true, l))
-				toBlock.setData((byte) 0);
+			else {
+				if(SafeCreeper.instance.getConfigManager().getOptionBoolean(w, "LavaControl", "InfiniteLava", false, true, l))
+					toBlock.setData((byte) 0);
+				
+				if(SafeCreeper.instance.getConfigManager().getOptionBoolean(w, "LavaControl", "SpreadUpwards", false, true, l)) {
+					BlockFace[] faces = new BlockFace[]{
+							BlockFace.NORTH, BlockFace.EAST,
+							BlockFace.SOUTH, BlockFace.WEST};
+					
+					for(BlockFace f : faces) {
+						Block relBlock = toBlock.getRelative(f);
+						Block aboveBlock = relBlock.getRelative(BlockFace.UP);
+						
+						if(!relBlock.isLiquid() && !relBlock.isEmpty()) {
+							if(aboveBlock.isEmpty())
+								aboveBlock.setType(b.getType());
+						}
+					}
+				}
+			}
 			break;
 		
 		default:
