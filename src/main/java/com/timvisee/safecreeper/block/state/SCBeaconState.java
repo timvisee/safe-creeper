3660191700385ea3d88b96de7d4816fe60ca5941
Tim Visée <timvisee@gmail.com>
2015-11-20 18:16:35 +0100

    Updated beacon state code

--- src/main/java/com/timvisee/safecreeper/block/state/SCBeaconState.java
+++ src/main/java/com/timvisee/safecreeper/block/state/SCBeaconState.java
@@ -1,20 +1,14 @@
 package com.timvisee.safecreeper.block.state;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.timvisee.safecreeper.SafeCreeper;
-import org.bukkit.Bukkit;
 import org.bukkit.block.Beacon;
 import org.bukkit.block.Block;
 import org.bukkit.inventory.Inventory;
-import org.bukkit.inventory.ItemStack;
 
 public class SCBeaconState extends SCBlockState {
 	
-	List<ItemStack> contents = new ArrayList<ItemStack>();
-	private int beaconInvSize;
-	
+	public int primaryEffectId = 0;
+	public int secondaryEffectId = 0;
+
 	/**
 	 * Constructor
 	 * @param b Beacon block
@@ -22,20 +16,6 @@ public class SCBeaconState extends SCBlockState {
 	public SCBeaconState(Beacon b) {
 		// Construct the parent class
 		super(b.getBlock());
-		
-		// Get the beacon state
-        //BeaconState bs = new BeaconState(b.getBlock());
-
-		/* // Store the beacon contents
-		this.contents.clear();
-		for(ItemStack entry : inv.getContents())
-			if(entry != null)
-				this.contents.add(entry.clone());
-			else
-				this.contents.add(null);
-			
-		// Store the beacon inventory size
-		this.beaconInvSize = inv.getSize();*/
     }
 	
 	/**
@@ -52,43 +32,6 @@ public class SCBeaconState extends SCBlockState {
 	 */
 	public Beacon getBeacon() {
 		return (Beacon) getBlock().getState();
-	}
-	
-	/**
-	 * Get the beacon contents
-	 */
-	public List<ItemStack> getContents() {
-		return this.contents;
-	}
-	
-	/**
-	 * Set the beacon contents
-	 * @param contents Beacon contents
-	 */
-	public void setContents(List<ItemStack> contents) {
-		this.contents = contents;
-	}
-	
-	/**
-	 * Get the beacons inventory size
-	 * @return Beacon inventory size
-	 */
-	public int getBeaconInventorySize() {
-		return this.beaconInvSize;
-	}
-	
-	/**
-	 * Set the beacon contents
-	 * @param contents Beacon contents
-	 */
-	public void setContents(ItemStack[] contents) {
-		this.contents.clear();
-		for(ItemStack entry : contents) {
-			if(entry != null)
-				this.contents.add(entry.clone());
-			else
-				this.contents.add(null);
-		}
 	}
 	
 	/**
@@ -109,16 +52,9 @@ public class SCBeaconState extends SCBlockState {
 		// Get the beacon
 		Beacon b = getBeacon();
 		
-		// Get the inventory of the beacon
-		Inventory inv = b.getInventory();
-		
-		// Make sure the beacon and the inventory instance are not null
-		if(b == null || inv == null)
-			return false;
-		
-		// Put the item back in the chest
+		/* // Put the item back in the chest
 		for(int i = 0; i < inv.getSize(); i++)
-			inv.setItem(i, this.contents.get(i));
+			inv.setItem(i, this.contents.get(i));*/
 		
 		// Update the beacon
 		b.update();
