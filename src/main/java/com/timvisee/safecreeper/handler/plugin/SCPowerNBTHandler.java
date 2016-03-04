1f93656482fee9495a20e6556ffd1595ab6e28d7
Tim Vis'ee <timvisee@gmail.com>
2016-03-04 09:07:28 +0100

    Created handler for PowerNBT plugin

---
+++ src/main/java/com/timvisee/safecreeper/handler/plugin/SCPowerNBTHandler.java
@@ -0,0 +1,31 @@
+package com.timvisee.safecreeper.handler.plugin;
+
+import com.timvisee.safecreeper.SCLogger;
+
+public class SCPowerNBTHandler extends SCPluginHandler {
+
+    /**
+     * Constructor
+     *
+     * @param pluginName
+     * @param log        SCLogger
+     */
+    public SCPowerNBTHandler(String pluginName, SCLogger log) {
+        super(pluginName, log);
+    }
+
+    @Override
+    public void hook() {
+
+    }
+
+    @Override
+    public boolean isHooked() {
+        return false;
+    }
+
+    @Override
+    public void unhook() {
+
+    }
+}
