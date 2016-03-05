747de00e8694b1f803191819eb790620cd11efbd
Tim Vis'ee <timvisee@gmail.com>
2016-03-05 09:12:23 +0100

    Completed hook handler class for the PowerNBT plugin

--- src/main/java/com/timvisee/safecreeper/handler/plugin/SCPowerNBTHandler.java
+++ src/main/java/com/timvisee/safecreeper/handler/plugin/SCPowerNBTHandler.java
@@ -1,31 +1,50 @@
 package com.timvisee.safecreeper.handler.plugin;
 
 import com.timvisee.safecreeper.SCLogger;
+import me.dpohvar.powernbt.api.NBTManager;
 
 public class SCPowerNBTHandler extends SCPluginHandler {
 
     /**
-     * Constructor
+     * The name of the plugin that is hooked.
+     */
+    private static final String PLUGIN_NAME = "PowerNBT";
+
+    /**
+     * A flag to track whether the PowerNBT plugin is hooked or not.
+     */
+    private boolean hooked = false;
+
+    /**
+     * Constructor.
      *
-     * @param pluginName
      * @param log        SCLogger
      */
-    public SCPowerNBTHandler(String pluginName, SCLogger log) {
-        super(pluginName, log);
+    public SCPowerNBTHandler(SCLogger log) {
+        super(PLUGIN_NAME, log);
     }
 
     @Override
     public void hook() {
-
+        this.hooked = true;
     }
 
     @Override
     public boolean isHooked() {
-        return false;
+        return this.hooked & getNBTManager() != null;
     }
 
     @Override
     public void unhook() {
+        this.hooked = false;
+    }
 
+    /**
+     * Get the NBT Manager from the PowerNBT API.
+     *
+     * @return The NBT Manager instance, or null if the plugin isn't properly hooked.
+     */
+    public NBTManager getNBTManager() {
+        return NBTManager.getInstance();
     }
 }
