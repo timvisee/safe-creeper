1442c15a8314be4bea5116d1347bb70c185c2b44
Tim Visée <timvisee@gmail.com>
2016-03-08 12:54:25 +0100

    Added some missing JavaDocs, created method for permissions system types to check whether they are a plugin or not

--- src/main/java/com/timvisee/safecreeper/permission/PermissionsManager.java
+++ src/main/java/com/timvisee/safecreeper/permission/PermissionsManager.java
@@ -942,6 +942,10 @@ public class PermissionsManager {
         return removeGroups(player, groupNames);
     }
 
+    /**
+     * The various permission system types.
+     * This is used to identify all the permission system types that are supported by the permissions manager.
+     */
     public enum PermissionsSystemType {
 
         /**
@@ -1021,6 +1025,16 @@ public class PermissionsManager {
          */
         public String getPluginName() {
             return this.pluginName;
+        }
+
+        /**
+         * Check whether this type is a real permissions system.
+         * NONE will return false.
+         *
+         * @return True if this is a permissions system.
+         */
+        public boolean isPermissionsSystem() {
+            return this != NONE;
         }
     }
 }
